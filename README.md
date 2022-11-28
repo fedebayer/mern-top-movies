f191092b4c325cdd30bfe6c1c1c3be56ae4edc12
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-28 15:38:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Environment variables
+
+- MONGODB_URI
+
+## Setup
+
+- run `npm i && npm run dev` for both client and server side to start the app
---
+++ server/index.js
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./config.js";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 500;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
