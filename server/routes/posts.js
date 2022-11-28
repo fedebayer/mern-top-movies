260099bd0fae1a14b7eba737f5c9fe7400ab2d85
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-28 15:59:39 -0300

    Added routes to server and structure of client

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { createPost, getPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("THIS WORKS!");
-});
+router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
